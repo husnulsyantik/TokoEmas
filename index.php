a99efc5ef5b3e5a0ab60a59e23078c96f151e6bc
Zakia <zakiyatulm35@gmail.com>
2017-11-27 10:45:07 +0700

    profil company

--- index.php
+++ index.php
@@ -5,13 +5,39 @@
  <!-- Page Header-->
           <header class="page-header">
             <div class="container-fluid">
-              <h2 class="no-margin-bottom">Dasboard</h2>
+              <h2 class="no-margin-bottom">HOME</h2>
             </div>
           </header>
-<!-- Dashboard Header Section    -->
-                <div class="col-lg-12"> 
-             <br><br>
-<br><br><br><br><br><br><br><br><br><br><br></div>
+ <!-- Forms Section-->
+          <section class="forms"> 
+            <div class="container-fluid">
+              <div class="row">
+                
+                <!-- Form Elements -->
+                <div class="col-lg-12">
+                  <div class="card">
+                    <div class="card-close">
+                         </div>
+                    
+					 <div class="card-body">
+					 
+                     
+
+                    <center> Visi</center><br>
+                    <p> Menjadi toko emas yang terbaik dan terpercaya yang selalu mengutamakan kualitas produk dan pelayanan yang ramah, jujur dan cepat.</p><br>
+                    <center> Misi</center><br>  
+                    <p>1. Menyediakan produk yang terbaik dan model-model terbaru</p>
+                    <p>2. Menjamin kepuasan pelanggan dengan pelayanan yang diberikan</p>
+                    <p>3. Perbaikan yang berkelanjutan</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+</section>
+
+                
+<br><br><br><br>
 <?php 
 	include 'footer.php';
     
