8b303aa145438247acf646046a61c55da9061458
3brothers\Rahman <rahmanpurwanto1996@gmail.com>
2017-11-29 04:51:55 +0700

    penambahan css dan form login, maaf proses blm krn dbnya ga bisa

---
+++ login.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Bootstrap Material Admin by Bootstrapious.com</title>
+      <link rel="stylesheet" type="text/css" href="css/demo.css"> <!-- login-->
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
+    <!-- Bootstrap CSS-->
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!-- Fontastic Custom icon font-->
+    <link rel="stylesheet" href="css/fontastic.css">
+    <!-- Font Awesome CSS-->
+    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+    <!-- Google fonts - Poppins -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,700">
+    <!-- theme stylesheet-->
+    <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
+    <!-- Custom stylesheet - for your changes-->
+    <link rel="stylesheet" href="css/custom.css">
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="favicon.png">
+    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+  </head>
+    <body>
+    <div class="page login-page">
+      <div class="container d-flex align-items-center">
+       <div class="form-holder has-shadow">
+          <div class="row">
+            <!-- Logo & Information Panel-->
+            <div class="col-lg-6">
+              <div class="info d-flex align-items-center">
+                  <div class="msg-profile"> <img src="img/tokoemas1.jpg" alt="..." height="400" width="500"></div>
+                <div class="content">
+                  <div class="logo">
+                <!--    <h1>TOKO </h1>-->
+                  </div>
+                  <p>.</p>
+                </div>
+              </div>
+            </div>
+            <!-- Form Panel    -->
+            <div class="col-lg-6 bg-white">
+              <div class="form d-flex align-items-top">
+                <div class="content">
+                 
+                        
+<div class="navbar-wrapper"></div>
+<h1 >Login Sebagai ?</h1> <br/>
+<div id="closed"></div>
+     <a href="#popup" class="btn btn-primary">Owner</a> 
+    <a href="#popup" class="btn btn-primary">Pengawas Toko</a>
+    <a href="#popup" class="btn btn-primary">Admin</a>
+<div class="popup-wrapper" id="popup">
+	<div class="popup-container">
+		<form action="" method="post" class="popup-form">
+			<h2>Masukan Password Anda</h2>
+			<p><br/>
+			<strong></strong></p>
+			<div class="input-group">
+    <!--    <a><input type="email" name="email" placeholder="Email Address"><br></a>-->
+		<p><input type="password" name="password" placeholder="password"><br></p>
+        <p><input type="submit" value="Submit" name=""><br></p>
+                
+			</div>
+		</form>
+		<a class="popup-close" href="#closed">X</a>
+	</div>
+</div>
+
+         </div>      
+        </div>
+      </div>
+      <div class="copyrights text-center">
+        <p>Design by <a href="https://bootstrapious.com/admin-templates" class="external">Bootstrapious</a></p>
+        <!-- Please do not remove the backlink to us unless you support further theme's development at https://bootstrapious.com/donate. It is part of the license conditions. Thank you for understanding :)-->
+      </div>
+    </div>
+            <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="vendor/popper.js/umd/popper.min.js"> </script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="vendor/jquery.cookie/jquery.cookie.js"> </script>
+    <script src="vendor/jquery-validation/jquery.validate.min.js"></script>
+    <script src="js/front.js"></script>
+  </body>
+</html>
