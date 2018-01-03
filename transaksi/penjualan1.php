9bec90120e55dfee0681a2b4692156438271fd69
DESKTOP-IMGK100\ASUS <ilhammuhammad9945@gmail.com>
2018-01-03 20:59:16 +0700

    proses input data penjualan ke database

--- transaksi/penjualan1.php
+++ transaksi/penjualan1.php
@@ -1,5 +1,18 @@
 <?php 
 	include '../header.php';
+include '../koneksi.php';
+
+$query = "SELECT max(idPenjualan) as maxKode FROM penjualan";
+$hasil = mysqli_query($conn,$query);
+$data  = mysqli_fetch_array($hasil);
+$kodemember = $data['maxKode'];
+
+$noUrut = (int) substr($kodemember, 3, 3);
+
+$noUrut++;
+
+$char = "T";
+$newID = $char . sprintf("%03s", $noUrut);
     
 ?>
           <!-- Page Header-->
@@ -25,78 +38,123 @@
 					 <div class="card-body">
 					 <p>Memasukkan kode barang, berat timbangan, kadar dan harga.<p>
                          
-                      <form class="form-horizontal" name="trans">
+                      <form class="form-horizontal" method="post">
                                             
                         <div class="line"></div>
+                          <div class="form-group row">
+                          <label class="col-sm-3 form-control-label">ID Penjualan</label>
+                              <div class="col-sm-6">
+                                <input type="text" name="idpenj" class="form-control form-control-success" id="kdtoko" value="<?php echo $newID; ?>" readonly>
+                              </div>
+                            </div>
+                          <div class="form-group row">
+                          <label class="col-sm-3 form-control-label">ID Toko</label>
+                              <div class="col-sm-6">
+                                <input type="text"  name="kdtoko" class="form-control form-control-success" id="kdtoko">
+                              </div>
+                            </div>
                        <div class="form-group row">
                           <label class="col-sm-3 form-control-label">Kode Barang</label>
-                          <div class="col-sm-6 select">
-                            <select name="account" class="form-control">
-                              <option>001</option>
-                              <option>002</option>
-                              <option>003</option>
-                              <option>004</option>
-                              <option>005</option>
-                              
-                              </select>
-                          </div>
+                           <div class="col-sm-6">
+                          <input type="text"  name="kdbarang" class="form-control form-control-success" id="kdbarang" placeholder="Masukkan Kode Barang" onkeyup="isiOtomatis(this.value)" onchange="isiOtomatis(this.value)">
+                           </div>
                             </div>
-                           <div class="form-group row">
-                      <label class="col-sm-3 form-control-label">Berat Timbangan </label>
+                        <div class="form-group row">
+                      <label class="col-sm-3 form-control-label">Berat Timbangan (gram)</label>
                       <div class="col-sm-6">
-                        <input type="text" name="berat" class="form-control form-control-success">
+                        <input type="text" name="berat" class="form-control form-control-success" id="berat">
                       </div>
                     </div>
                           <div class="form-group row">
                           <label class="col-sm-3 form-control-label">Kadar</label>
                           <div class="col-sm-6 select">
-                            <select name="account" class="form-control">
-                              <option>BM</option>
-                              <option>CTK</option>
-                              <option>AYU</option>
-                              </select>
+                            <input type="text" name="kadar" class="form-control form-control-success" id="kadar">
                           </div>
                             </div>
                         <div class="form-group row">
                           <label class="col-sm-3 form-control-label">Harga</label>
                           <div class="col-sm-3 select">
-                            <input id="htoko" type="radio" value="" name="harga">
+                            <input id="htoko" type="radio" value="" name="pilihan">
                             <label for="option">Harga sesuai harga toko</label>
                           </div>
                           <div class="col-sm-3">
-                             <input type="text" name="berat" class="form-control form-control-success" value="Rp.500.000" id="toko">
+                             <input type="text" name="harga" class="form-control form-control-success" id="toko">
                           </div>
                         </div>
                           
                         <div class="form-group row">
                           <label class="col-sm-3 form-control-label"></label>
                           <div class="col-sm-3 select">
-                            <input id="htawar" type="radio" name="harga">
+                            <input id="htawar" type="radio" name="pilihan">
                             <label for="option">Harga Tawaran</label>
                           </div>
                           <div class="col-sm-3">
-                             <input type="text" name="berat" class="form-control form-control-success" id="tawar">
+                             <input type="text" name="harga" class="form-control form-control-success" id="tawar" value="0" onchange="hitung()">
                           </div>
                         </div>
+                          
+                    <div class="form-group row">
+                      <label class="col-sm-3 form-control-label">Harga Total</label>
+                      <div class="col-sm-6">
+                        <input type="text" name="hargatot" class="form-control form-control-success" id="hargatot">
+                      </div>
+                    </div>
+                          <div class="form-group row">
+                      <label class="col-sm-3 form-control-label">ID USER</label>
+                      <div class="col-sm-6">
+                        <input type="text" name="iduser" class="form-control form-control-success">
+                      </div>
+                    </div>
      
                         <div class="line"></div>
                         <div class="form-group row">
                           <div class="col-sm-4 offset-sm-3">
-						   <a href="#"><input type="button" value="&nbsp;&nbsp;Batal&nbsp;&nbsp;" type="submit" class="btn btn-secondary"></a>&nbsp;&nbsp;&nbsp;
-                          <a href="penjualan2.php"><input type="button" value="&nbsp;&nbsp;Lanjut&nbsp;>>&nbsp;&nbsp;" type="submit" class="btn btn-primary"></a>
+<!--						   <a href="#"><input type="button" value="&nbsp;&nbsp;Batal&nbsp;&nbsp;" type="submit" class="btn btn-secondary"></a>&nbsp;&nbsp;&nbsp;-->
+<!--                          <a href="penjualan2.php"><input type="button" value="&nbsp;&nbsp;Lanjut&nbsp;>>&nbsp;&nbsp;" type="submit" class="btn btn-primary"></a>-->
+                          <input type="submit" value="&nbsp;&nbsp;Simpan&nbsp;&nbsp;&nbsp;" class="btn btn-primary" name="simpan">
                           </div> 
                       </div>
                          </form>
+                         
+<!--                         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>-->
+                         <script type="text/javascript">
+                             
+            function isiOtomatis(kodbarang){
+                $("#kdbarang").change(function(){
+                //var kodbarang = $("#kdbarang").val();
+                $.ajax({
+                    url: 'tampil.php',
+                    data: "kode="+kodbarang,
+                
+                    success:function(data){
+                        var json = data,
+                        obj = JSON.parse(json);
+                        $('#berat').val(obj.berat);
+                        $('#toko').val(obj.harga);
+                        $('#kadar').val(obj.kadar);
+                       // $('#toko').val(data['harga1']);
+                    }
+                });
+                });
+                
+            }
+                        </script>
+                         
                          
                          <script>
                             $('#htoko').click(function(){
                                 $('#tawar').prop('disabled',true);
                                 $('#toko').prop('disabled',false);
+                            document.getElementById('hargatot').value=document.getElementById('toko').value*document.getElementById('berat').value;
                             })
+                             
                             $('#htawar').click(function(){
                                 $('#tawar').prop('disabled',false);
                                 $('#toko').prop('disabled',true);
                             })
+                             function hitung(){
+                             document.getElementById('hargatot').value=document.getElementById('tawar').value*document.getElementById('berat').value;
+                                 }
                         </script>
 							</div>
                         </div>
@@ -104,6 +162,34 @@
               </div>
               </div>
 </section>
+
+<?php
+    if(isset($_POST['simpan'])){
+$aa=$_POST['idpenj'];
+$a=$_POST['kdtoko'];
+$b=$_POST['kdbarang'];
+$c=$_POST['berat'];
+$d=$_POST['kadar'];
+$e=$_POST['harga'];
+$f=$_POST['hargatot'];
+$g=$_POST['iduser'];
+date_default_timezone_set('Asia/Jakarta');
+$tanggal = date('Y-m-d H:i:s');
+
+$sql="insert into penjualan (idPenjualan, berat_now, harga_gram, harga_total, tanggal_penjualan, idbarang, id_user, idtoko) values ('$aa','$c','$e','$f','$tanggal','$b','$g','$a')";
+$query_sql = mysqli_query($conn, $sql);
+    if($query_sql){
+		echo "<script language=\"JavaScript\">\n";
+		echo "alert(\"Data Berhasil Disimpan...!!\");\n";
+		echo "window.location=\"penjualan1.php\" ";
+		echo "</script>";
+	}else{ 
+		echo "<script language=\"JavaScript\">\n";
+		echo "alert(\"Data Gagal Disimpan...!!\");\n";
+		echo "window.location=\"penjualan1.php\" ";
+		echo "</script>";}
+}
+?>
                      
           <?php 
 	include '../footer.php';
---
+++ transaksi/tampil.php
@@ -0,0 +1,36 @@
+<?php
+include "../koneksi.php";
+
+$kode = $_GET['kode'];
+$query = mysqli_query($conn, "select id_kadar, berat, harga_jual from barang INNER JOIN harga USING (id_kadar) where idBarang = '$kode'");
+$mahasiswa = mysqli_fetch_array($query);
+$id_kadar = $mahasiswa['id_kadar'];
+$nama_kadar = mysqli_query($conn, "select kode from kadar where idkadar = '$id_kadar'");
+$baru = mysqli_fetch_array($nama_kadar);
+
+$data = array(
+            'berat'    =>  $mahasiswa['berat'],
+            'harga'    =>  $mahasiswa['harga_jual'],
+            'kadar'    =>  $baru['kode'],);
+
+
+echo json_encode($data)
+?>
+
+
+
+
+
+
+
+
+
+
+<?php
+//id_kadar = $mahasiswa['id_kadar'];
+//$nama_kadar = mysqli_query($conn, "select kode from kadar where idkadar = '$id_kadar'");
+//$baru = mysqli_fetch_array($nama_kadar);
+/*$data = array(
+            'berat1'   =>  $mahasiswa['berat'],
+            'kadar1'   =>  $mahasiswa['idbaki'],
+            'harga1'   =>  $mahasiswa['harga_jual'],);*/
